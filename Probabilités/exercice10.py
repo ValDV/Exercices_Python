cce23b17249c57efd8d8b56b49877fabae3c84a9
ValDV <valentinrsr20@gmail.com>
2024-03-27 14:56:10 +0100

    méthode monte carlo

---
+++ Probabilités/exercice10.py
@@ -0,0 +1,10 @@
+from random import*
+def monte_carlo():
+    n=1000
+    c=0
+    for i in range(n):
+        x=random()
+        y=random()
+        if x**2+y**2 < 1:
+            c+=1
+    return c/n
