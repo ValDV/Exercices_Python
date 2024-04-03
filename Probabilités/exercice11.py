3b2d74f189e68962320907c951cb2bf4603267cd
ValDV <valentinrsr20@gmail.com>
2024-04-03 15:24:51 +0200

    proba11 fin

--- Probabilités/exercice11.py
+++ Probabilités/exercice11.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def compte(lettre, texte):
     compteur = 0
     for L in texte.lower():
@@ -8,14 +10,20 @@ def compte(lettre, texte):
 def frequences(texte):
     freq_dict = {}
     texte = texte.lower()
-
     for lettre in texte:
         if lettre.isalpha():
             if lettre in freq_dict:
                 freq_dict[lettre] += 1
             else:
                 freq_dict[lettre] = 1
-                
     freq_list = list(freq_dict.items())
     freq_list.sort(key=lambda x: x[0])
     return freq_list
+
+def afficher_diagramme(freq_list):
+    lettres, frequences = zip(*freq_list)
+    plt.bar(lettres, frequences)
+    plt.xlabel('Lettres')
+    plt.ylabel('Fréquences')
+    plt.title('Diagramme en bâtons des fréquences d\'apparition des lettres')
+    plt.show()
