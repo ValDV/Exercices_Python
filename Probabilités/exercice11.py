2629aa20034d61a2569067d4c46ebfd871a78b86
ValDV <valentinrsr20@gmail.com>
2024-04-03 15:23:10 +0200

    proba11 update

--- Probabilités/exercice11.py
+++ Probabilités/exercice11.py
@@ -1,6 +1,21 @@
-def compte(lettre,texte):
+def compte(lettre, texte):
     compteur = 0
-    for L in texte.lower() :
-        if :
-        compteur =
-    return 
+    for L in texte.lower():
+        if L == lettre:
+            compteur += 1
+    return compteur
+
+def frequences(texte):
+    freq_dict = {}
+    texte = texte.lower()
+
+    for lettre in texte:
+        if lettre.isalpha():
+            if lettre in freq_dict:
+                freq_dict[lettre] += 1
+            else:
+                freq_dict[lettre] = 1
+                
+    freq_list = list(freq_dict.items())
+    freq_list.sort(key=lambda x: x[0])
+    return freq_list
